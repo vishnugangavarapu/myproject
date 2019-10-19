4890870a986edb91e790cc5be3a51c4183c7bb86
vishnugangavarapu <chowdary.gvvn520@outlook.com>
2019-10-19 19:32:24 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+func setTimeout() {
+	if *timeoutOpt != 0 {
+		timeout = *timeoutOpt
+	}
+}
+
+var (
+	showList   = kingpin.Flag("list", "Show available speedtest.net servers").Short('l').Bool()
+	serverIds  = kingpin.Flag("server", "Select server id to speedtest").Short('s').Ints()
+	timeoutOpt = kingpin.Flag("timeout", "Define timeout seconds. Default: 10 sec").Short('t').Int()
+	timeout    = 10
+)
+
+func main() {
+	kingpin.Version("1.0.3")
+	kingpin.Parse()
+
+	setTimeout()
+
+	user := FetchUserInfo()
+	user.Show()
+
+	list := FetchServerList(user)
+	if *showList {
+		list.Show()
+		return
+	}
+
+	targets := list.FindServer(*serverIds)
+	targets.StartTest()
+	targets.ShowResult()
+}
